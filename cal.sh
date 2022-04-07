aead6278b32301d387d8f12121c1bdaebf31034c
hwanhee11 <qgg1978@naver.com>
2022-04-07 20:38:20 +0900

    cal.sh upload

---
+++ cal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+arr[0]=$(<num1.txt)
+arr[1]=$(<num2.txt)
+
+PS3='select menu : '
+select menu in "add" "sub" "div" "mul"
+do
+    if [[ "$menu" = "add" ]]; then
+        echo -e "\nnum1 : ${arr[0]}"; echo "num2 : ${arr[1]}"; echo "op : $menu"
+        let result=${arr[0]}+${arr[1]}; echo "result : $result"; exit 1
+    elif [[ "$menu" = "sub" ]]; then
+        echo -e "\nnum1 : ${arr[0]}"; echo "num2 : ${arr[1]}"; echo "op : $menu"
+        let result=${arr[0]}-${arr[1]}; echo "result : $result"; exit 1
+    elif [[ "$menu" = "div" ]]; then
+        echo -e "\nnum1 : ${arr[0]}"; echo "num2 : ${arr[1]}"; echo "op : $menu"
+        let result=${arr[0]}/${arr[1]}; echo "result : $result"; exit 1
+    elif [[ "$menu" = "mul" ]]; then
+        echo -e "\nnum1 : ${arr[0]}"; echo "num2 : ${arr[1]}"; echo "op : $menu"
+        let result=${arr[0]}*${arr[1]}; echo "result : $result"; exit 1
+    fi
+done
